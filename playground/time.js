1aac5027154b9edeb2cb7d27d33a87e94711d40c
Kit Macleod <kit.macleod@gmail.com>
2017-09-15 06:50:49 +0100

    Add mustache.js for templates

---
+++ playground/time.js
@@ -0,0 +1,23 @@
+var moment = require('moment');
+
+//Jan 1st 1970 00:00:00 UTC am
+
+// var date = new Date();
+// var months = ['Jan', 'Feb']
+// console.log(date.getMonth());
+
+// var date = moment();
+// //date.add(1, 'years').subtract(9, 'months');
+// console.log(date.format('MMM Do, YYYY'))
+
+
+//10:35 am
+//padded for minutes, and unpaded for hours, hour 1-12
+// new Date().getTime()
+var someTimestamp = moment().valueOf();
+console.log(someTimestamp)
+
+
+var createdAt = 1234;
+var date = moment(createdAt);
+console.log(date.format('h:mm a'))
