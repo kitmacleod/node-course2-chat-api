a915342ab57204681eaf0952e26ec829b10aa058
Kit Macleod <kitmacleod@gmail.com>
2017-09-17 10:59:17 +0100

    Added  user test

--- public/js/chat.js
+++ public/js/chat.js
@@ -13,19 +13,35 @@ function scrollToBottom () {
 
   if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
     messages.scrollTop(scrollHeight);
-  }
 }
 
-
-
+}
 
 socket.on('connect', function () {
-  console.log('Connected to server');
+  var params = jQuery.deparam(window.location.search);
+
+  socket.emit('join', params, function (err) {
+if (err) {
+  alert(err);
+  window.location.href ='/';
+} else {
+  console.log('No error');
+
+}
+  });
 });
 
 socket.on('disconnect', function () {
   console.log ('Disconnected from server');
 });
+
+socket.on('updateUserList', function (users) {
+  console.log('Users list', users);
+});
+
+
+
+
 
 socket.on('newMessage', function(message) {
 var formattedTime = moment(message.createdAt).format('h:mm a');
